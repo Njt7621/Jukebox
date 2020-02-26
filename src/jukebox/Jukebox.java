22599e7aabf533009edfd13e0c790d371f64d4d7
Nikkia Theodule <njt7621@rit.edu>
2020-02-25 23:57:55 -0500

    Tried my best >_<

--- src/jukebox/Jukebox.java
+++ src/jukebox/Jukebox.java
@@ -1,6 +1,5 @@
 package jukebox;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
@@ -8,40 +7,66 @@ import java.util.*;
 public class Jukebox {
     public static void main(String[] args) {
         Jukebox jukebox = new Jukebox();
+        //runSim();
+        prtStats();
         }
 
-    public void runSim (String filename) {
+    /**
+     * A Song whose instances can be compared by artist, then by song.
+     */
+    public void runSim (String filename) throws FileNotFoundException {
         HashSet<String> h_set = new HashSet<String>();
-        HashMap<String, String> h_map = new HashMap<String, String>();
+        HashMap<Song, Integer> h_map = new HashMap<Song, Integer>();
 
         //read data from file, line by line
         String line;
         Scanner in = new Scanner(new File(filename));
         while(in.hasNext()){
+            line= in.nextLine();
             String[] values = line.split("<SEP>",4);
             //access individual values
+            String song = values[0];
+            String artist = values[1];
+            Song t = new Song( song, artist);
+            h_map.put(t, 0);
 
-            //set.add() the song object created from values
         }
         //run simulation now
-        //System.out.println("Simulation took" + # + "second/s" );
-        //System.out.println("Number of simulations run: " + #);
+        System.out.println("Jukebox of " + "songs starts rockin'...");
+        System.out.println("Simulation took " + " second/s" );
+        System.out.println("Number of simulations run: " );
     }
         //for (String key : hash_map.keySet()) {
             //System.out.println(key + "Should be a string" + hash_map.get(key));
         //}
 
+    /**
+     * A Song whose instances can be compared by artist, then by song.
+     */
     public int getStats () {
-            return 0;
+        //to acess random songs:
+        //hashmap.get(randomnumber)
+
+        //get total number of songs played:
+
+
+        //get average num of songs per simulation:
+        //(totSong/50000)
+
+        //get most played song:
+        return 0;
 
     }
 
-    public String prtStats () {
-        System.out.println("Total number of songs played: " +);
-        System.out.println("Average number of songs played per simulation to get duplicate: " +);
-        System.out.println("Most played song: " +);
-        System.out.println("All songs alphabetically by " +)
-        System.out.println( + "with" +  + "plays");
+    /**
+     * A Song whose instances can be compared by artist, then by song.
+     */
+    public static String prtStats() {
+        System.out.println("Total number of songs played: " );
+        System.out.println("Average number of songs played per simulation to get duplicate: " );
+        System.out.println("Most played song: " );
+        System.out.println("All songs alphabetically by " );
+        System.out.println( " with "+" plays");
         return "foo";
     }
 }
